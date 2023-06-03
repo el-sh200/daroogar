6e609502f1da74f9a9f2aeb3222c58435e94f55b
Sharifia <el.sh200@gmail.com>
2023-06-03 14:07:30 +0330

    Connect to postgresql

--- daroogar/settings.py
+++ daroogar/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'daroogar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'daroogar',
+        'USER': 'root',
+        'PASSWORD': 'WOli6cjOvXTyFuLQXmebwxJy',
+        'HOST': 'daroogar',
+        'PORT': '5432',
     }
 }
 
